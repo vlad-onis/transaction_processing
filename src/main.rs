f704b3df70afb1d70d4d9d2d65c48512fe3c9fdb
vladhateganos <vladhateganos@gmail.com>
2021-08-22 21:26:31 +0300

    TP-02 - Initial commig * Parsed input * Validated input, must be a csv file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+mod cli_arguments;
+
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    // args not available after this line - ownership
+    let input = cli_arguments::parse_input(args);
+
+    if let Some(file) = input {
+        println!(
+            "Input file: {}",
+            file.into_os_string().into_string().unwrap()
+        );
+    }
 }
---
+++ src/utils/cli_arguments_utils.rs
@@ -0,0 +1,39 @@
+use std::ffi::OsStr;
+use std::path;
+
+pub fn parse_input(args: Vec<String>) -> Option<path::PathBuf> {
+    let input_csv_file = match args.get(1) {
+        None => {
+            println!(
+                "No input provided, please refer to the README.md \
+                file for input examples"
+            );
+            return None;
+        }
+        Some(file) => file,
+    };
+
+    let mut input_path = path::PathBuf::new();
+    input_path.push(input_csv_file);
+
+    if input_path.exists() {
+        let file_extension = input_path.extension().and_then(OsStr::to_str);
+
+        return if let Some("csv") = file_extension {
+            Some(input_path)
+        } else {
+            println!(
+                "Input is NOT a CSV file please refer to the README.md \
+                file for input example"
+            );
+            None
+        };
+    } else {
+        println!(
+            "Input file does NOT exist please refer to the README.md \");
+            file for input examples"
+        );
+    }
+
+    None
+}
